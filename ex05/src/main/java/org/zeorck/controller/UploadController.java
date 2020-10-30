7f32fc310968050e5d223d773d17493c20483090
powerseunghun <size411@naver.com>
2020-10-30 22:55:13 +0900

    Add Detail Process

--- ex05/src/main/java/org/zeorck/controller/UploadController.java
+++ ex05/src/main/java/org/zeorck/controller/UploadController.java
@@ -1,6 +1,8 @@
 package org.zeorck.controller;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -43,20 +45,38 @@ public class UploadController {
 		log.info("upload ajax");
 	}
 	
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		String str = sdf.format(date);
+		return str.replace("-", File.separator);
+	}
+	
 	@PostMapping("/uploadAjaxAction")
 	public void uploadAjaxPost(MultipartFile[] uploadFile) {
 		log.info("update ajax post.....");
 		
 		String uploadFolder = "/Users/powerseunghun/Desktop/Developer/upload/temp";
 		
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("upload path : {}", uploadPath);
+		
+		if (uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		// make yyyy/MM/dd folder
+		
 		for (MultipartFile multipartFile : uploadFile) {
 			log.info("--------------------------");
 			log.info("Upload File Name: " + multipartFile.getOriginalFilename());
+			log.info("Upload File Size: " + multipartFile.getSize());
 			
 			String uploadFileName = multipartFile.getOriginalFilename();
 			uploadFileName = uploadFileName.substring(uploadFileName.indexOf("\\") + 1);
 			
-			File saveFile = new File(uploadFolder, uploadFileName);
+//			File saveFile = new File(uploadFolder, uploadFileName);
+			File saveFile = new File(uploadPath, uploadFileName);
 			
 			try {
 				multipartFile.transferTo(saveFile);
--- ex05/src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ ex05/src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -15,6 +15,22 @@
 	<button id="uploadBtn">Upload</button>
 	<script>
 		$(document).ready(function() {
+			var regex = new RegExp("(.*?)\.(exe|png|sh|zip|alz)$");
+			var maxSize = 5242880;
+			
+			function checkExtension(fileName, fileSize) {
+				if(fileSize >= maxSize) {
+					alert("파일 사이즈 초과");
+					return false;
+				}
+				
+				if (regex.test(fileName)) {
+					alert("해당 종류의 파일은 업로드할 수 없습니다.");
+					return false;
+				}
+				return true;
+			}
+			
 			$("#uploadBtn").on("click", function(e) {
 				var formData = new FormData();
 				var inputFile = $("input[name='uploadFile']");
@@ -24,6 +40,10 @@
 				
 				// add filedate to formdata
 				for (var i = 0; i < files.length; i++) {
+					if (!checkExtension(files[i].name, files[i].size)) {
+						return false;
+					}
+					
 					formData.append("uploadFile", files[i]);
 				}
 				
