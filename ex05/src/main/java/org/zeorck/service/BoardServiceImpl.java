1a61aad7dda1c310ee7e9b0b6d55208e308ad520
powerseunghun <size411@naver.com>
2020-11-01 19:54:55 +0900

    Attachment Modify Process Implements

--- ex05/src/main/java/org/zeorck/service/BoardServiceImpl.java
+++ ex05/src/main/java/org/zeorck/service/BoardServiceImpl.java
@@ -54,7 +54,17 @@ public class BoardServiceImpl implements BoardService{
 	public boolean modify(BoardVO board) {
 		log.info("modify......." + board);
 		
-		return mapper.update(board) == 1;
+		attachMapper.deleteAll(board.getBno());
+		boolean modifyResult = mapper.update(board) == 1;
+		
+		if (modifyResult && board.getAttachList() != null && board.getAttachList().size() > 0) {
+			board.getAttachList().forEach(attach -> {
+				attach.setBno(board.getBno());
+				attachMapper.insert(attach);
+			});
+		}
+		
+		return modifyResult;
 	}
 
 	@Override
