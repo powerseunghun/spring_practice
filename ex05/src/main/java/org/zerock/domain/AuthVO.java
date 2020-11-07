96766607654c9bd8683b3d36e3c3a10dec8b5dff
powerseunghun <size411@naver.com>
2020-11-07 23:14:48 +0900

    Merge Base Project

---
+++ ex05/src/main/java/org/zerock/domain/AuthVO.java
@@ -0,0 +1,15 @@
+package org.zerock.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthVO {
+	private String userid;
+	private String auth;
+}
