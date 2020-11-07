ba9014ee156cba77a3b2b19520de27953a669607
powerseunghun <size411@naver.com>
2020-11-08 00:43:22 +0900

    Security Fix

--- ex05/src/main/java/org/zerock/controller/BoardController.java
+++ ex05/src/main/java/org/zerock/controller/BoardController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -54,6 +55,7 @@ public class BoardController {
 	}
 	
 	@PostMapping("/register")
+	@PreAuthorize("isAuthenticated()")
 	public String register(BoardVO board, RedirectAttributes rttr) {
 		log.info("==========================");
 		log.info("register: " + board);
@@ -70,6 +72,7 @@ public class BoardController {
 	}
 	
 	@GetMapping("/register")
+	@PreAuthorize("isAuthenticated()")
 	public void register() {
 		
 	}
